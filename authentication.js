66a389ba03905d69c5cb01566d22a6ea582083d5
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-08-16 20:45:41 -0400

    created passport authentication service

---
+++ authentication.js
@@ -0,0 +1,40 @@
+var passport = require('passport');
+var mongoose = require('mongoose');
+var User = require('./models/user.js');
+
+module.exports = {
+  signup: signup,
+  login: login
+};
+
+function signup(req, res){
+  var user = new User(req.body);
+
+  user.setPassword(req.body.password);
+  user.save(function(err){
+    if(err){
+      return res.status(500).json({
+        msg: 'error!'
+      })
+    }
+    return res.status(200).json({
+      msg: 'success'
+    })
+  });
+}
+function login(req, res){
+  passport.authenticate('local', function(err, user, info){
+    if(err){
+      return res.status(500).json({
+        msg: 'Authentication Failed'
+      })
+    }
+    if(user){
+      return res.status(200).json({
+        msg: 'Authentication Succeeded'
+      })
+    } else {
+      return res.status(401).json(info);
+    }
+  })
+}
